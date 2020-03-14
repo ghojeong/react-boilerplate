df0ad847d948c9c0c686a30fa364146f2b589e9e
고정완 (Can) <jw.gho@ecubelabs.com>
2020-03-14 10:56:20 +0900

    API_ENDPOINT 수정

--- src/app/actions/extraActions.ts
+++ src/app/actions/extraActions.ts
@@ -1,5 +1,6 @@
 import { createAction, ActionType } from "typesafe-actions";
 
+// NOTE: 리덕스 스토어를 초기화하는 액션
 export const resetAction = createAction("RESET")();
 
 export type ExtraActions = ActionType<typeof resetAction>;
