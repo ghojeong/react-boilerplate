009e32963c3e2649852e04a9346451835395ea35
고정완 (Can) <jw.gho@ecubelabs.com>
2020-03-16 17:06:20 +0900

    불필요한 describe 삭제

--- src/app/epics/fetchProblemsEpic.spec.ts
+++ src/app/epics/fetchProblemsEpic.spec.ts
@@ -9,54 +9,52 @@ import * as feProblems from "app/assets/fe-problems.json";
 const problems = feProblems.data;
 jest.mock("app/services");
 
-describe("epic 테스트", () => {
-  describe("fetchProblemsEpic 테스트", () => {
-    const problemService = mocked(service.problemService, true);
+describe("fetchProblemsEpic 테스트", () => {
+  const problemService = mocked(service.problemService, true);
 
-    test("problems 조회가 성공하면 success 액션이 발생해야 한다.", done => {
-      // <!-- mock
-      problemService.getProblems.mockReturnValueOnce(of(problems));
-      // -->
-      const actions$ = ActionsObservable.of(fetchProblemsAsyncAction.request());
-      const state$ = new StateObservable(new Subject(), {});
-      const dependencies = { problemService };
-      const actualActions: Action[] = [];
+  test("problems 조회가 성공하면 success 액션이 발생해야 한다.", done => {
+    // <!-- mock
+    problemService.getProblems.mockReturnValueOnce(of(problems));
+    // -->
+    const actions$ = ActionsObservable.of(fetchProblemsAsyncAction.request());
+    const state$ = new StateObservable(new Subject(), {});
+    const dependencies = { problemService };
+    const actualActions: Action[] = [];
 
-      fetchProblemsEpic(actions$, state$, dependencies).subscribe({
-        next: (action: Action) => actualActions.push(action),
-        complete: () => {
-          expect(actualActions).toEqual([
-            fetchProblemsAsyncAction.success(problems)
-          ]);
-          done();
-        }
-      });
+    fetchProblemsEpic(actions$, state$, dependencies).subscribe({
+      next: (action: Action) => actualActions.push(action),
+      complete: () => {
+        expect(actualActions).toEqual([
+          fetchProblemsAsyncAction.success(problems)
+        ]);
+        done();
+      }
     });
+  });
 
-    test("problems 조회가 실패하면 failure 액션과 스낵바 액션이 발생해야 한다.", done => {
-      // <!-- mock
-      problemService.getProblems = jest
-        .fn()
-        .mockImplementation(() => throwError(new Error("Get Problems Error")));
-      // -->
-      const actions$ = ActionsObservable.of(fetchProblemsAsyncAction.request());
-      const state$ = new StateObservable(new Subject(), {});
-      const dependencies = { problemService };
-      const actualActions: Action[] = [];
+  test("problems 조회가 실패하면 failure 액션과 스낵바 액션이 발생해야 한다.", done => {
+    // <!-- mock
+    problemService.getProblems = jest
+      .fn()
+      .mockImplementation(() => throwError(new Error("Get Problems Error")));
+    // -->
+    const actions$ = ActionsObservable.of(fetchProblemsAsyncAction.request());
+    const state$ = new StateObservable(new Subject(), {});
+    const dependencies = { problemService };
+    const actualActions: Action[] = [];
 
-      fetchProblemsEpic(actions$, state$, dependencies).subscribe({
-        next: action => actualActions.push(action),
-        complete: () => {
-          expect(actualActions).toEqual([
-            fetchProblemsAsyncAction.failure("Error: Get Problems Error"),
-            enqueueSnackbarAction({
-              message: "Get Problems Error",
-              variant: "error"
-            })
-          ]);
-          done();
-        }
-      });
+    fetchProblemsEpic(actions$, state$, dependencies).subscribe({
+      next: action => actualActions.push(action),
+      complete: () => {
+        expect(actualActions).toEqual([
+          fetchProblemsAsyncAction.failure("Error: Get Problems Error"),
+          enqueueSnackbarAction({
+            message: "Get Problems Error",
+            variant: "error"
+          })
+        ]);
+        done();
+      }
     });
   });
 });
--- src/app/epics/fetchSimilarsEpic.spec.ts
+++ src/app/epics/fetchSimilarsEpic.spec.ts
@@ -9,54 +9,52 @@ import * as feSimilars from "app/assets/fe-similars.json";
 const similars = feSimilars.data;
 jest.mock("app/services");
 
-describe("epic 테스트", () => {
-  describe("fetchSimilarsEpic 테스트", () => {
-    const similarService = mocked(service.similarService, true);
+describe("fetchSimilarsEpic 테스트", () => {
+  const similarService = mocked(service.similarService, true);
 
-    test("similars 조회가 성공하면 success 액션이 발생해야 한다.", done => {
-      // <!-- mock
-      similarService.getSimilars.mockReturnValueOnce(of(similars));
-      // -->
-      const actions$ = ActionsObservable.of(fetchSimilarsAsyncAction.request());
-      const state$ = new StateObservable(new Subject(), {});
-      const dependencies = { similarService };
-      const actualActions: Action[] = [];
+  test("similars 조회가 성공하면 success 액션이 발생해야 한다.", done => {
+    // <!-- mock
+    similarService.getSimilars.mockReturnValueOnce(of(similars));
+    // -->
+    const actions$ = ActionsObservable.of(fetchSimilarsAsyncAction.request());
+    const state$ = new StateObservable(new Subject(), {});
+    const dependencies = { similarService };
+    const actualActions: Action[] = [];
 
-      fetchSimilarsEpic(actions$, state$, dependencies).subscribe({
-        next: (action: Action) => actualActions.push(action),
-        complete: () => {
-          expect(actualActions).toEqual([
-            fetchSimilarsAsyncAction.success(similars)
-          ]);
-          done();
-        }
-      });
+    fetchSimilarsEpic(actions$, state$, dependencies).subscribe({
+      next: (action: Action) => actualActions.push(action),
+      complete: () => {
+        expect(actualActions).toEqual([
+          fetchSimilarsAsyncAction.success(similars)
+        ]);
+        done();
+      }
     });
+  });
 
-    test("similars 조회가 실패하면 failure 액션과 스낵바 액션이 발생해야 한다.", done => {
-      // <!-- mock
-      similarService.getSimilars = jest
-        .fn()
-        .mockImplementation(() => throwError(new Error("Get Similars Error")));
-      // -->
-      const actions$ = ActionsObservable.of(fetchSimilarsAsyncAction.request());
-      const state$ = new StateObservable(new Subject(), {});
-      const dependencies = { similarService };
-      const actualActions: Action[] = [];
+  test("similars 조회가 실패하면 failure 액션과 스낵바 액션이 발생해야 한다.", done => {
+    // <!-- mock
+    similarService.getSimilars = jest
+      .fn()
+      .mockImplementation(() => throwError(new Error("Get Similars Error")));
+    // -->
+    const actions$ = ActionsObservable.of(fetchSimilarsAsyncAction.request());
+    const state$ = new StateObservable(new Subject(), {});
+    const dependencies = { similarService };
+    const actualActions: Action[] = [];
 
-      fetchSimilarsEpic(actions$, state$, dependencies).subscribe({
-        next: action => actualActions.push(action),
-        complete: () => {
-          expect(actualActions).toEqual([
-            fetchSimilarsAsyncAction.failure("Error: Get Similars Error"),
-            enqueueSnackbarAction({
-              message: "Get Similars Error",
-              variant: "error"
-            })
-          ]);
-          done();
-        }
-      });
+    fetchSimilarsEpic(actions$, state$, dependencies).subscribe({
+      next: action => actualActions.push(action),
+      complete: () => {
+        expect(actualActions).toEqual([
+          fetchSimilarsAsyncAction.failure("Error: Get Similars Error"),
+          enqueueSnackbarAction({
+            message: "Get Similars Error",
+            variant: "error"
+          })
+        ]);
+        done();
+      }
     });
   });
 });
